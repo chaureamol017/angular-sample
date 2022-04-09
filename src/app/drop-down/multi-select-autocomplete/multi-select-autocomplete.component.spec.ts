0cd3ac4b86212f465d0976e6a3f4cdc81062714c
chaurea <amol.chaure@sincrodigital.com>
2022-04-09 13:24:25 +0530

    Refactor

--- src/app/drop-down/multi-select-autocomplete/multi-select-autocomplete.component.spec.ts
+++ src/app/drop-down/multi-select-autocomplete/multi-select-autocomplete.component.spec.ts
@@ -1,14 +1,26 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MultiSelectData } from 'src/app/models/data-model';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MultiSelectAutocompleteComponent } from './multi-select-autocomplete.component';
 
 describe('MultiSelectAutocompleteComponent', () => {
   let component: MultiSelectAutocompleteComponent;
   let fixture: ComponentFixture<MultiSelectAutocompleteComponent>;
+  const dataSource: MultiSelectData[] = [new MultiSelectData(1, '2020 New Chevy'), new MultiSelectData(2, 'Cadillac')];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MultiSelectAutocompleteComponent ]
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+      ],
+      declarations: [MultiSelectAutocompleteComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
     })
     .compileComponents();
   }));
@@ -16,10 +28,21 @@ describe('MultiSelectAutocompleteComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MultiSelectAutocompleteComponent);
     component = fixture.componentInstance;
+    component.placeHolder = '';
+    component.options = dataSource;
+    component.onClosedEvent = () => { };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should check filter with undefined', () => {
+    const filterQuery = undefined;
+    component.filterData(filterQuery);
+    expect(component.filteredOptions.length).toBe(2);
+  });
+
 });
